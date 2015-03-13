b651d01c2c539b28881bebc8cb0d6c59db75f251
Wscript <dick.li.sheng@gmail.com>
2015-03-13 14:50:20 +0800

    完成计时部分的功能

---
+++ Elementary Arithmetic/FormMain.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Elementary_Arithmetic
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelEqual = new System.Windows.Forms.Label();
+            this.labelNumber2 = new System.Windows.Forms.Label();
+            this.labelSymbol = new System.Windows.Forms.Label();
+            this.labelNumber1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.textBoxMinutes = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelScore
+            // 
+            this.labelScore.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.labelScore.Location = new System.Drawing.Point(262, 301);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(128, 50);
+            this.labelScore.TabIndex = 7;
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTime
+            // 
+            this.labelTime.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTime.ForeColor = System.Drawing.Color.Red;
+            this.labelTime.Location = new System.Drawing.Point(40, 301);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(142, 50);
+            this.labelTime.TabIndex = 6;
+            this.labelTime.Text = "00:00";
+            this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxResult);
+            this.groupBox2.Controls.Add(this.labelEqual);
+            this.groupBox2.Controls.Add(this.labelNumber2);
+            this.groupBox2.Controls.Add(this.labelSymbol);
+            this.groupBox2.Controls.Add(this.labelNumber1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 158);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 113);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxResult.Location = new System.Drawing.Point(312, 37);
+            this.textBoxResult.MaxLength = 2;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(86, 62);
+            this.textBoxResult.TabIndex = 9;
+            this.textBoxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // labelEqual
+            // 
+            this.labelEqual.AutoSize = true;
+            this.labelEqual.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelEqual.Location = new System.Drawing.Point(250, 40);
+            this.labelEqual.Name = "labelEqual";
+            this.labelEqual.Size = new System.Drawing.Size(44, 48);
+            this.labelEqual.TabIndex = 8;
+            this.labelEqual.Text = "=";
+            // 
+            // labelNumber2
+            // 
+            this.labelNumber2.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelNumber2.Location = new System.Drawing.Point(164, 40);
+            this.labelNumber2.Name = "labelNumber2";
+            this.labelNumber2.Size = new System.Drawing.Size(68, 48);
+            this.labelNumber2.TabIndex = 7;
+            this.labelNumber2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelSymbol
+            // 
+            this.labelSymbol.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSymbol.Location = new System.Drawing.Point(102, 40);
+            this.labelSymbol.Name = "labelSymbol";
+            this.labelSymbol.Size = new System.Drawing.Size(44, 48);
+            this.labelSymbol.TabIndex = 6;
+            // 
+            // labelNumber1
+            // 
+            this.labelNumber1.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelNumber1.Location = new System.Drawing.Point(16, 40);
+            this.labelNumber1.Name = "labelNumber1";
+            this.labelNumber1.Size = new System.Drawing.Size(68, 48);
+            this.labelNumber1.TabIndex = 5;
+            this.labelNumber1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonStart);
+            this.groupBox1.Controls.Add(this.labelMinutes);
+            this.groupBox1.Controls.Add(this.textBoxMinutes);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 118);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonStart.Location = new System.Drawing.Point(272, 30);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(128, 62);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "开始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelMinutes
+            // 
+            this.labelMinutes.AutoSize = true;
+            this.labelMinutes.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelMinutes.Location = new System.Drawing.Point(100, 37);
+            this.labelMinutes.Name = "labelMinutes";
+            this.labelMinutes.Size = new System.Drawing.Size(140, 48);
+            this.labelMinutes.TabIndex = 4;
+            this.labelMinutes.Text = "分/题";
+            // 
+            // textBoxMinutes
+            // 
+            this.textBoxMinutes.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxMinutes.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBoxMinutes.Location = new System.Drawing.Point(8, 30);
+            this.textBoxMinutes.MaxLength = 2;
+            this.textBoxMinutes.Name = "textBoxMinutes";
+            this.textBoxMinutes.Size = new System.Drawing.Size(86, 62);
+            this.textBoxMinutes.TabIndex = 0;
+            this.textBoxMinutes.Text = "100";
+            this.textBoxMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxMinutes.TextChanged += new System.EventHandler(this.textBoxMinutes_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 376);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(406, 113);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(236, 70);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 25);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "闯关模式";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 70);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(112, 25);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "计时模式";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(346, 30);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(50, 25);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "除";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(236, 30);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(50, 25);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "乘";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(126, 30);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(50, 25);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "减";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 30);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 25);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "加";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 501);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "四则运算";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label labelEqual;
+        private System.Windows.Forms.Label labelNumber2;
+        private System.Windows.Forms.Label labelSymbol;
+        private System.Windows.Forms.Label labelNumber1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.TextBox textBoxMinutes;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Elementary Arithmetic/FormMain.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Elementary_Arithmetic
+{
+    public partial class FormMain : Form
+    {
+        static int timerTickCounter = 0;
+        static Random randomizer = new Random ();
+        static int intAnswer;
+
+        public FormMain ()
+        {
+            InitializeComponent ();
+        }
+
+        private void FormMain_Load (object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+            checkBox2.Checked = true;
+            checkBox3.Checked = true;
+            checkBox4.Checked = true;
+            radioButton1.Checked = true;
+            textBoxMinutes.Enabled = true;
+            textBoxMinutes.Focus ();
+            textBoxMinutes.Text = "";
+            textBoxResult.Enabled = false;
+            this.AcceptButton = buttonStart;
+
+
+            if (!Directory.Exists ("d:\\log"))
+            {
+                Directory.CreateDirectory ("d:\\log");
+            }
+
+            if (File.Exists ("d:\\log\\四则运算测试" + DateTime.Now.Year.ToString () + "-" + DateTime.Now.Month.ToString () + "-" + DateTime.Now.Day.ToString () + ".log"))
+            {
+                Log ("测试开始!", "d:\\log\\", "四则运算测试" + DateTime.Now.Year.ToString () + "-" + DateTime.Now.Month.ToString () + "-" + DateTime.Now.Day.ToString () + ".log");
+            }
+            else
+            {
+                FileStream newLogFile = new FileStream ("d:\\log\\四则运算测试" +
+                                                        DateTime.Now.Year.ToString () + "-" +
+                                                        DateTime.Now.Month.ToString () + "-" +
+                                                        DateTime.Now.Day.ToString () + ".log",
+                                                        FileMode.OpenOrCreate);
+                newLogFile.Close ();
+                Log ("测试开始!", "d:\\log\\", "四则运算测试" + DateTime.Now.Year.ToString () + "-" + DateTime.Now.Month.ToString () + "-" + DateTime.Now.Day.ToString () + ".log");
+            }
+        }
+
+        private int NewQuiz ()
+        {
+            int intNumber1, intNumber2,intOperator,intResult;
+
+            
+            intResult = -1;
+            do
+            {
+                intOperator = randomizer.Next (4) + 1;
+                switch (intOperator)
+                {
+                    case 1:
+                        if (checkBox1.Checked == true)
+                        {
+                            labelSymbol.Text = "+";
+                            do
+                            {
+                                //随机生成2个1-99的整数
+                                intNumber1 = randomizer.Next (99) + 1;
+                                intNumber2 = randomizer.Next (99) + 1;
+                            } while (intNumber1 + intNumber2 >= 100);   //确保2个整数的和小于100
+                            labelNumber1.Text = intNumber1.ToString ();
+                            labelNumber2.Text = intNumber2.ToString ();
+                            intResult = intNumber1 + intNumber2;
+                        }
+                        break;
+                    case 2:
+                        if (checkBox2.Checked == true)
+                        {
+                            labelSymbol.Text = "-";
+                            //随机生成2个1-99的整数
+                            intNumber1 = randomizer.Next (99) + 1;
+                            intNumber2 = randomizer.Next (99) + 1;
+                            if (intNumber1 > intNumber2)
+                            {
+                                labelNumber1.Text = intNumber1.ToString ();
+                                labelNumber2.Text = intNumber2.ToString ();
+                                intResult = intNumber1 - intNumber2;
+                            }
+                            else
+                            {
+                                labelNumber1.Text = intNumber2.ToString ();
+                                labelNumber2.Text = intNumber1.ToString ();
+                                intResult = intNumber2 - intNumber1;
+                            }
+                        }
+                        break;
+                    case 3:
+                        if (checkBox3.Checked == true)
+                        {
+                            labelSymbol.Text = "x";
+                            //随机生成2个1-9的整数
+                            intNumber1 = randomizer.Next (9) + 1;
+                            intNumber2 = randomizer.Next (9) + 1;
+                            labelNumber1.Text = intNumber1.ToString ();
+                            labelNumber2.Text = intNumber2.ToString ();
+                            intResult = intNumber1 * intNumber2;
+                        }
+                        break;
+                    case 4:
+                        if (checkBox4.Checked == true)
+                        {
+                            labelSymbol.Text = "÷";
+                            //随机生成2个1-9的整数
+                            intNumber1 = randomizer.Next (9) + 1;
+                            intNumber2 = randomizer.Next (9) + 1;
+                            labelNumber1.Text = (intNumber1 * intNumber2).ToString();
+                            labelNumber2.Text = intNumber2.ToString ();
+                            intResult = intNumber1;
+                        }
+                        break;
+                }
+            } while (intResult == -1);
+
+            return (intResult);
+        }
+
+        static void Log (string logMessage, string logFolder, string logFilename)
+        {
+            using (StreamWriter w = File.AppendText (logFolder + logFilename))
+            {
+                w.Write ("\r\n");
+                w.Write ("{0} {1}", DateTime.Now.ToLongDateString (), DateTime.Now.ToLongTimeString ());
+                w.Write ("  :{0}", logMessage);
+            }
+        }
+
+        private void timer_Tick (object sender, EventArgs e)
+        {
+            int intMinutes = 0;
+            string strRemainMinutes = "";
+            string strRemainSeconds = "";
+            bool isNumber = int.TryParse (textBoxMinutes.Text, out intMinutes);
+
+            timerTickCounter = timerTickCounter + 1;
+
+            int intRemainMinutes = (intMinutes * 60 - timerTickCounter) / 60;
+            int intRemainSeconds = (intMinutes * 60 - timerTickCounter) % 60;
+            if (intRemainMinutes < 10)
+            {
+                strRemainMinutes = "0" + intRemainMinutes.ToString ();
+            }
+            else
+            {
+                strRemainMinutes = intRemainMinutes.ToString ();
+            }
+            if (intRemainSeconds < 10)
+            {
+                strRemainSeconds = "0" + intRemainSeconds.ToString ();
+            }
+            else
+            {
+                strRemainSeconds = intRemainSeconds.ToString ();
+            }
+
+            labelTime.Text = strRemainMinutes + ":" + strRemainSeconds;
+
+            if (timerTickCounter / 60 == intMinutes)
+            {
+                timer.Stop ();
+                timerTickCounter = 0;           //停止计时器，并将激活次数清零
+                textBoxMinutes.Enabled = true;
+                textBoxResult.Enabled = false;
+                buttonStart.Enabled = true;
+                textBoxMinutes.Focus ();
+                Log (textBoxMinutes.Text + "分钟四则运算测试完成，共计" + labelScore.Text + "题.",
+                    "d:\\log\\",
+                    "四则运算测试" + DateTime.Now.Year.ToString () + "-" + DateTime.Now.Month.ToString () + "-" + DateTime.Now.Day.ToString () + ".log");
+            }
+
+        }
+        private void buttonStart_Click (object sender, EventArgs e)
+        {
+            checkBox1.Enabled = false;
+            checkBox2.Enabled = false;
+            checkBox3.Enabled = false;
+            checkBox4.Enabled = false;
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            textBoxMinutes.Enabled = false;
+            textBoxResult.Enabled = true;
+            textBoxResult.Text = "";
+            textBoxResult.Focus ();
+
+            if (radioButton1.Checked == true)
+            {
+                int intMinutes = 0;
+                if (int.TryParse (textBoxMinutes.Text, out intMinutes))
+                {
+                    labelTime.Text = intMinutes.ToString () + ":00";
+                    timer.Interval = 1000;
+                    timer.Start ();
+                    labelScore.Text = "0";
+                    buttonStart.Enabled = false;
+                    intAnswer = NewQuiz ();
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void textBoxResult_TextChanged (object sender, EventArgs e)
+        {
+            int intResult, intScore;
+            if (int.TryParse (textBoxResult.Text, out intResult))
+            {
+                if (intAnswer == intResult)
+                {
+                    intScore = System.Convert.ToInt32 (labelScore.Text);
+                    intScore = intScore + 1;
+                    labelScore.Text = intScore.ToString ();
+                    textBoxResult.Text = "";
+                    intAnswer = NewQuiz ();
+                }
+            }
+        }
+
+        private void textBoxMinutes_TextChanged (object sender, EventArgs e)
+        {
+            Mode_Changed ();
+        }
+
+        private void radioButton2_CheckedChanged (object sender, EventArgs e)
+        {
+            Mode_Changed ();
+        }
+
+        private void radioButton1_CheckedChanged (object sender, EventArgs e)
+        {
+            Mode_Changed();
+        }
+
+        private void Mode_Changed ()
+        {
+            if (radioButton1.Checked == true)
+            {
+                int intMinutes = 0;
+                if (int.TryParse (textBoxMinutes.Text, out intMinutes))
+                {
+                    labelTime.Text = intMinutes.ToString () + ":00";
+                    labelMinutes.Text = "分钟";
+                }
+            }
+            else
+            {
+                labelTime.Text = "00:00";
+                labelMinutes.Text = "题";
+            }
+        }
+    }
+}
